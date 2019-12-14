88188dc9bfd12c0407590532f92fedb7ceb23ed8
SnowMelody <45004592+SnowMelody@users.noreply.github.com>
2019-12-14 19:05:00 +0800

    Add files via upload

---
+++ ABT_updated/launcher.bat
@@ -0,0 +1,4 @@
+set mypath = %~dp0
+echo mypath
+Rscript %mypath%run.R
+cmd /k
---
+++ ABT_updated/run.R
@@ -0,0 +1,3 @@
+require(shiny)
+folder_address = paste0(getwd(),"\\ABioTrans_updated.R")
+runApp(folder_address, launch.browser=TRUE)
